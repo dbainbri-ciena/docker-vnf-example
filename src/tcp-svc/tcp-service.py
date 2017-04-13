4285c31a534af02d2a7212eef5394d9c7b332ec4
David K. Bainbridge <dbainbri@ciena.com>
2017-04-13 13:29:04 -0700

    update to flush after output to make logs more realtime

--- src/tcp-svc/tcp-service.py
+++ src/tcp-svc/tcp-service.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python 
 
-import socket, os, threading
+import sys, socket, os, threading
 
 RECEIVE_IP = os.environ.get('RECEIVE_IP', '0.0.0.0')
 RECEIVE_PORT = int(os.environ.get('RECEIVE_PORT', 5080))
@@ -9,14 +9,17 @@ MESSAGE = os.environ.get('MESSAGE', 'Good morning')
 print "RX ON: %s:%s" % (RECEIVE_IP, RECEIVE_PORT)
 print "MSG  : %s" % MESSAGE
 print "-----"
+sys.stdout.flush()
 
 def conversation(client_socket):
     data = client_socket.recv(1024)
     print "+RX : %s:%s -> %s" % (addr[0], addr[1], data)
     print "+TX : %s:%s -> %s" % (addr[0], addr[1], MESSAGE)
+    sys.stdout.flush()
     client_socket.send(MESSAGE)
     client_socket.close()
     print "+DX : %s:%s" % (addr[0], addr[1])
+    sys.stdout.flush()
 
 receive_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 receive_sock.bind((RECEIVE_IP, RECEIVE_PORT))
@@ -25,4 +28,5 @@ receive_sock.listen(5)
 while True:
     (client_socket, addr) = receive_sock.accept()
     print "CX  : %s:%s" % (addr[0], addr[1])
+    sys.stdout.flush()
     threading.Thread(conversation(client_socket)).run()
--- src/udp-svc/udp-service.py
+++ src/udp-svc/udp-service.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python 
 
-import socket, os
+import sys, socket, os
 
 RECEIVE_IP = os.environ.get('RECEIVE_IP', '0.0.0.0')
 RECEIVE_PORT = int(os.environ.get('RECEIVE_PORT', 5068))
@@ -11,6 +11,7 @@ print "RX ON: %s:%s" % (RECEIVE_IP, RECEIVE_PORT)
 print "TX ON: :%s" % SEND_PORT
 print "MSG  : %s" % MESSAGE
 print "-----"
+sys.stdout.flush()
 
 receive_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 receive_sock.bind((RECEIVE_IP, RECEIVE_PORT))
@@ -20,4 +21,5 @@ while True:
     data, addr = receive_sock.recvfrom(1024) # buffer size is 1024 bytes
     print "RX  : %s:%s -> %s" % (addr[0], addr[1], data)
     print "+TX : %s:%s -> %s" % (addr[0], SEND_PORT, MESSAGE)
+    sys.stdout.flush()
     send_sock.sendto(MESSAGE, (addr[0], SEND_PORT))
